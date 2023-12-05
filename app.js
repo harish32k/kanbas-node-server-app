c746d4b8c76670a7b4ff4336f4fab876a83e70ea
Harish Akula <harish.akula32k@gmail.com>
2023-12-04 20:27:50 -0500

    Updated app for assignment 6

--- app.js
+++ app.js
@@ -6,12 +6,43 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import session from "express-session";
+
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
+);
 app.use(express.json());
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app)
+AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
